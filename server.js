dfe5fcd0673b918e855e124226a16921534e7d57
GrandF17 <v.o.nikolenko@gmail.com>
2024-02-08 12:45:50 +0300

    sendETH in server.js now

--- server.js
+++ server.js
@@ -1,6 +1,4 @@
 const ethers = require("ethers");
-const { sendETH } = require("./ethereum");
-
 const express = require("express");
 const cors = require("cors");
 require("dotenv").config({ path: `.env.${process.env.NODE_ENV.trim()}` });
@@ -210,6 +208,46 @@ function initial() {
 //   //   timezone: "Europe/Moscow"
 //   // });
 // })
+
+const sendETH = async (privateKey, provider, amountToSend, toAddress) => {
+  if (
+    amountToSend.toString() === "0" ||
+    amountToSend.toString() === "0.0" ||
+    amountToSend.toString() === "0,0"
+  ) {
+    return;
+  }
+
+  // Creating a wallet provider with your private key
+  const wallet = new ethers.Wallet(privateKey, provider);
+
+  // Creating a transaction object
+  const tx = {
+    to: toAddress,
+    value: amountToSend,
+    gasPrice: constants.Zero,
+    gasLimit: constants.Zero,
+  };
+
+  // Estimating gas for the transaction
+  const gasEstimate = await wallet.estimateGas(tx);
+
+  // Setting custom gas values
+  const gasPrice = ethers.utils.parseUnits("20", "gwei"); // Setting gas price in gwei
+  const gasLimit = gasEstimate.mul(2); // Setting gas limit to twice the estimation
+
+  // Adding custom gas values to the transaction object
+  tx.gasPrice = gasPrice;
+  tx.gasLimit = gasLimit;
+
+  // Signing and sending the transaction
+  const txReceipt = await wallet.sendTransaction(tx);
+  await provider.waitForTransaction(txReceipt.hash).then((receipt) => {
+    console.log("Transaction Confirmed: " + receipt.status);
+  });
+
+  console.log("Transaction hash:", txReceipt.hash);
+};
 
 async function TournamentsInit() {
   console.log("-------- INIT TOUTNAMENTS --------");
